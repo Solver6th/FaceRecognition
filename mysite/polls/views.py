5730ed378db702e4e75e5ca31fe7c83b482e7221
Solver6th <115849010+Solver6th@users.noreply.github.com>
2022-11-06 13:13:22 +0900

    설문 조사 앱 추가하기 12
    view.py 오류 고침
    스타일 시트 생성

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,13 +12,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(
-        pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
