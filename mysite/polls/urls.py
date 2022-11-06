34d2bd319091b1806ea52d01731beb2cfe7ff3bd
Solver6th <115849010+Solver6th@users.noreply.github.com>
2022-11-06 12:36:47 +0900

    설문 조사 앱 추가 9
    URL의 이름 공간 정하기

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -12,3 +12,11 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
