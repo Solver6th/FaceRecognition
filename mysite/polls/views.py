56dc5a72814603c6e05ed4013d7bf7609843164a
Solver6th <115849010+Solver6th@users.noreply.github.com>
2022-11-07 21:29:16 +0900

    설문 조사 앱 오류 수정
    view.py 수정 과정에서 생긴 오류

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -42,9 +42,6 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return render(request, 'polls/index.html', context)
-    return HttpResponse(template.render(context, request))
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
 
 def detail(request, question_id):
     try:
@@ -52,15 +49,13 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -76,6 +71,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-    return HttpResponse("You're voting on question %s." % question_id)
