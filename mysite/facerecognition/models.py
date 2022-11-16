5f45f9134fb23edd180d686b9a979a9557bc1fbc
Solver6th <115849010+Solver6th@users.noreply.github.com>
2022-11-17 00:01:42 +0900

    Django 이미지 업로드 및 출력하기 2
    models 설정

--- mysite/facerecognition/models.py
+++ mysite/facerecognition/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Face(models.Model):
+    image = models.ImageField(upload_to = "images/", null=True, blank=True)
+
+    def __str__(self):
+        return "face"
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'facerecognition.apps.FacerecognitionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
