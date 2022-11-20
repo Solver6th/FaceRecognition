4551034b64caf9955a4f114701a87c61a9e37688
Solver6th <115849010+Solver6th@users.noreply.github.com>
2022-11-20 09:37:02 +0900

    파이썬 OpenCV를 이용한 얼굴 인식 3
    얼굴 탐지 알고리즘 적용

--- OpenCV/main.py
+++ OpenCV/main.py
@@ -11,3 +11,15 @@ img = cv2.imread('sample.jpg')
 def imgDetector(img,cascade):
     img = cv2.resize(img, dsize=None, fx=0.5, fy=0.5)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#cascaed 얼굴 탐지 알고리즘
+#(입력 이미지, 이미지 피라미드 스케일 factor, 인접 객체 최소 거리 픽셀, 탐지 객첵 최소 크기)
+results = cascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5, minSize(20,20)) 
+
+#결과값 = 탐지된 객체의 경계상자 list
+for box in results:
+    #좌표 추출
+    x, y, w, h =  box
+    #경계 상자 그리기
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), thinckness=2)
+
