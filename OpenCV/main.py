68681c0b0eeaa3a90eae04aef8b505d03a9d878d
Solver6th <115849010+Solver6th@users.noreply.github.com>
2022-11-20 09:29:09 +0900

    파이썬 OpenCV를 이용한 얼굴 인식 2
    사진 불러오기

--- OpenCV/main.py
+++ OpenCV/main.py
@@ -4,3 +4,10 @@ import cv2
 cascade_filename = 'haarcascade_frontalface_alt.xml'
 #모델 불러오기
 cascade = cv2.CascadeClassifier(cascade_filename)
+
+#사진 파일
+img = cv2.imread('sample.jpg')
+#사진 출력
+def imgDetector(img,cascade):
+    img = cv2.resize(img, dsize=None, fx=0.5, fy=0.5)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
