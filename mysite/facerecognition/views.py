a3b33b1fd29abafea9277533a2f059c8a45571b6
sangbin2806 <115852021+sangbin2806@users.noreply.github.com>
2022-11-25 23:58:55 +0900

    facerecognition 1125
    facerecognition 11/25일자 추가한것들

--- mysite/facerecognition/views.py
+++ mysite/facerecognition/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from .forms import FaceForm
+from .models import Face
 
 def index(request):
     return render(request, 'facerecognition/index.html')
+
+class FaceImage(TemplateView):
+    form = FaceForm
+    template_name = 'facerecognition/face_image.html'
+
+    def post(self, request, *args , **kwargs):
+        form = FaceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse_lazy('home', kwargs={'pk': pk}))
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)     
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+class FaceImageDisplay(DetailView):
+    model = Face
+    template_name = 'facerecognition/face_image_display.html'
+    context_object_name = 'face'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
