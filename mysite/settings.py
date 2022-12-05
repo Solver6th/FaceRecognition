a0595020fc8c045d2386aae1c9e294a20726b763
sangbin2806 <115852021+sangbin2806@users.noreply.github.com>
2022-12-05 23:10:18 +0900

    최종 수정
    최종 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+
+CSRF_TRUSTED_ORIGINS = ['https://zealous-bush-40bf7199970f48b688ea932bb3ebdc58.azurewebsites.net']
